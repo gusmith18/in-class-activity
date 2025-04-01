35ab4749331cb32b2bb7b150e503b0b6276cfbd3
Gavin Smith <gusaster76@gmail.com>
2025-04-01 10:26:07 -0400

    Update even_odd_site.java

--- programming files/even_odd_site.java
+++ programming files/even_odd_site.java
@@ -4,6 +4,7 @@ import java.io.*;
 
 public class even_odd_site {
     public static void main(String[] args) throws IOException {
+        //this writes a list of even and odd numbers to a file seperated
         BufferedWriter file = new BufferedWriter(new FileWriter("numbers.html"));
         file.write("<html>\n<head>\n<title>List of Numbers</title>\n</head>\n<body>\n");
         file.write("<table>\n<tr><th>Even Numbers</th><th>Odd Numbers</th></tr>\n");
@@ -17,6 +18,7 @@ public class even_odd_site {
         }
         file.write("</table>\n</body>\n</html>");
         file.close();
+        //this prints out the contents of the file just written
         BufferedReader reader = new BufferedReader(new FileReader("numbers.html"));
         String line;
         while ((line = reader.readLine()) != null) {
